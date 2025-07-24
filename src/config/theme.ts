a1565f45d6858fdb29f9cf1bd1ccc2908cbf689e
F21BINFT1M01051 <f21binft1m01051@iub.edu.pk>
2025-07-24 20:47:33 +0500

    packages & structure

---
+++ src/config/theme.ts
@@ -0,0 +1,38 @@
+export const COLORS = {
+  gradient1: '#C64998',
+  gradient2: '#661348',
+  primary: '#2E303A',
+  black: '#000000',
+  skyBlue: '#1877F2',
+  borderColor: '#2E303A',
+  fieldColor: '#F2F2F2',
+  fieldBorder: '#DEDEDE',
+  placeholder: 'rgba(0,0,0,0.5)',
+  inputColor: '#1D211E',
+  focused: '#373A46',
+};
+
+export const IMAGES = {
+  logo: require('../assets/images/logo.png'),
+  onBoarding: require('../assets/images/onBoarding.png'),
+  headline: require('../assets/images/headline.png'),
+};
+
+export const ICONS = {
+  facebook: require('../assets/icons/facebook.png'),
+  apple: require('../assets/icons/ios.png'),
+  google: require('../assets/icons/google.png'),
+  user: require('../assets/icons/user.png'),
+};
+
+export const FONTS = {
+  headline: 'Caprasimo-Regular',
+  regular: 'SplineSans-Regular',
+  medium: 'SplineSans-Medium',
+  semiBold: 'SplineSans-SemiBold',
+  bold: 'SplineSans-Bold',
+  regular2: 'PublicSans-Regular',
+  medium2: 'PublicSans-Medium',
+  semiBold2: 'PublicSans-SemiBold',
+  bold2: 'PublicSans-Bold',
+};
